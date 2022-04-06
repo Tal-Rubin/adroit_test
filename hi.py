d7642c7f58dfc1b455b9c806ddd20462bd6af833
Tal Rubin <trubin@pppl.gov>
2022-04-06 14:26:01 -0400

    4

--- hi.py
+++ hi.py
@@ -4,10 +4,11 @@ import xraylib as xrl
 num_bins = 30
 
 bin_lims = np.linspace(0,300,num_bins +1) #kev
-bins = (bin_lims[1:]+bin_lims[:-1]) * 0.5
+bin_E = (bin_lims[1:]+bin_lims[:-1]) * 0.5
 
-bin_sigma_tot = np.zeros(bins.shape)
+bin_sigma_tot = np.zeros(num_bins)
 
-for b,bin in zip(bin_sigma_tot, bins):
-	b = xrl.CS_Total(92,bin)
+for i in range(num_bins):
+	bin_sigma_tot[i] = xrl.CS_Total(92,bin_E[i])
+
 print(bin_sigma_tot)
