55cd6807834de5c11bd5ec4c3c06a0656abf2867
Tal Rubin <trubin@pppl.gov>
2022-04-06 14:17:16 -0400

    3

--- hi.py
+++ hi.py
@@ -8,6 +8,6 @@ bins = (bin_lims[1:]+bin_lims[:-1]) * 0.5
 
 bin_sigma_tot = np.zeros(bins.shape)
 
-for b,bin in bin_sigma_tot, bins:
+for b,bin in zip(bin_sigma_tot, bins):
 	b = xrl.CS_Total(92,bin)
 print(bin_sigma_tot)
