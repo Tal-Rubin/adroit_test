8887e4ecf9d612a841a0805fe1866a192e6fd87d
Tal Rubin <trubin@pppl.gov>
2022-04-06 14:15:29 -0400

    2

--- hi.py
+++ hi.py
@@ -1,3 +1,13 @@
-print("hello world")
+import numpy as np
+import xraylib as xrl
 
-print("hello world again")
+num_bins = 30
+
+bin_lims = np.linspace(0,300,num_bins +1) #kev
+bins = (bin_lims[1:]+bin_lims[:-1]) * 0.5
+
+bin_sigma_tot = np.zeros(bins.shape)
+
+for b,bin in bin_sigma_tot, bins:
+	b = xrl.CS_Total(92,bin)
+print(bin_sigma_tot)
