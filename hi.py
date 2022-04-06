22acd11842e7bd6c3dbc4dfa61834918745f9685
Tal Rubin <trubin@pppl.gov>
2022-04-06 14:29:52 -0400

    6

--- hi.py
+++ hi.py
@@ -10,7 +10,7 @@ max_E = 600
 bin_lims = np.linspace(min_E, max_E ,num_bins +1) #kev
 bin_E = (bin_lims[1:]+bin_lims[:-1]) * 0.5
 
-bin_sigma_tot = np.zeros(bins.shape)
+bin_sigma_tot = np.zeros(num_bins)
 
 for i in range(num_bins):
 	bin_sigma_tot[i] = xrl.CS_Total(92,bin_E[i])
