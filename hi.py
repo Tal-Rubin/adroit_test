2b9573c0319f80b62606301c294ca1aeda610321
Tal Rubin <trubin@adroit-vis.princeton.edu>
2022-04-07 14:45:40 -0400

    7

--- hi.py
+++ hi.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import xraylib as xrl
 
-num_bins = 60
+num_bins = 200
 min_E = 0
 max_E = 600
 
@@ -18,5 +18,18 @@ def get_sigma_tot(element, bin_lims, order):
     	bin_sigma_tot[i] = xrl.CS_Total(element,0.5*(bin_lims[i]+bin_lims[i+1]))
     return bin_sigma_tot
 
-bin_sigma_tot = get_sigma_tot(92, bin_lims, 1)
+bin_sigma_tot = get_sigma_tot(6, bin_lims, 1)
+
+
+
+plt.figure()
 plt.plot(bin_E,bin_sigma_tot)
+
+
+plt.figure()
+plt.plot(bin_E,bin_sigma_tot*bin_E)
+
+plt.show()
+
+
+print(sum(bin_sigma_tot*bin_E)*(bin_lims[1]-bin_lims[0]))
